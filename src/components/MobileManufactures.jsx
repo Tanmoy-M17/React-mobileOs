1aed5d801e91bc41b9e36666123e940deb7120e2
Tanmoy-M17 <97627356+Tanmoy-M17@users.noreply.github.com>
2022-05-23 10:48:08 +0530

    done

---
+++ src/components/MobileManufactures.jsx
@@ -0,0 +1,13 @@
+import {React} from 'react';
+export const Mf=()=>{
+    return <div>
+        <h1>Mobile Manufactures</h1>
+        <ul type='square'>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+        </ul>
+           
+    </div>
+}
---
+++ src/components/MoboleOs.jsx
@@ -0,0 +1,13 @@
+import {React} from 'react';
+export const Os=()=>{
+    return <div id='os'>
+        <h1>Mobile Operating System</h1>
+        <ul>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>iPhone</li>
+            <li>Windows Phone</li>
+        </ul>
+        
+    </div>
+}
